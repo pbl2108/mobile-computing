29dc2980d9ced062413557dcc215aa513d8b0b34
Eddy <ewgarci@gmail.com>
2013-03-25 17:15:59 -0400

    added methods to compute jaccard similarities and output to file

--- SmaliParser.java
+++ SmaliParser.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.lucene.util.OpenBitSet;
@@ -22,82 +24,87 @@ public class SmaliParser {
 	public String hashMapFile = "C:\\Users\\Dfosak\\Documents\\GitHub\\mobile-computing\\smali-methods.txt";
 	public String outputFeatureFile = "C:\\Users\\Dfosak\\Documents\\GitHub\\mobile-computing\\testRun.txt";
 	public String outputBitVectorFile = "C:\\Users\\Dfosak\\Documents\\GitHub\\mobile-computing\\bitVectors.csv";
+	public String outputSimFile = "C:\\Users\\Dfosak\\Documents\\GitHub\\mobile-computing\\similarities.txt";
 	public File folder;
 	public HashMap<String, Integer> featuresHashMap;
+	public HashMap<String, OpenBitSet> bitVectorsHashMap;
+	public int bitVectorsCount;
 	public long recCount;
 	public long unRecCount;
 	public long count;
+	public double jaccardThreshold = .70;
 	
 	
 	public SmaliParser(){
 		this.features = new ArrayList<String>();
 		this.folder = new File(this.folderRoot);
 		this.featuresHashMap = new HashMap<String, Integer>(); 
+		this.bitVectorsHashMap = new HashMap<String, OpenBitSet>(); 
 		this.recCount = 0;
 		this.unRecCount = 0;
-		this.count=0;
+		this.count = 0;
+		this.bitVectorsCount = 0;
 	}
 	
 	 public static void main(String[] args) {
 		SmaliParser smaliParser = new SmaliParser();
 		smaliParser.createHashMap(smaliParser.hashMapFile);
-
 		
 		smaliParser.topLevelTraversal(smaliParser.folder);
 		
+		
 		System.out.println("Recognized: " + smaliParser.recCount + " Unrecognized: " + smaliParser.unRecCount + " Total: " + smaliParser.count );
-
+		smaliParser.compareBitVectors();
 		
 	 }
 	 
 	private void parseDelimitedFile(String filePath, OpenBitSet bitVector) throws Exception
 	{
 	  
-	  FileReader fr = new FileReader(filePath);
-	  BufferedReader br = new BufferedReader(fr);
-	  String currentRecord;
-	  String delimiter = "\\s+";
-	  String tokens[];
-	  String token;
+		FileReader fr = new FileReader(filePath);
+		BufferedReader br = new BufferedReader(fr);
+		String currentRecord;
+		String delimiter = "\\s+";
+		String tokens[];
+		String token;
+		  
+		File file = new File(this.outputFeatureFile);
 	  
+		// if file doesnt exists, then create it
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		 
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		BufferedWriter bw = new BufferedWriter(fw);
 	  
-	  
-	  File file = new File(this.outputFeatureFile);
-	  
-	// if file doesnt exists, then create it
-	if (!file.exists()) {
-		file.createNewFile();
-	}
-	 
-	FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
-	BufferedWriter bw = new BufferedWriter(fw);
-  
-	  while((currentRecord = br.readLine()) != null){		
-		if (currentRecord.contains("invoke")){
-			tokens =  currentRecord.split(delimiter);
-			token = tokens[tokens.length-1];
-					
-			//System.out.println(token);
-			if (featuresHashMap.containsKey(token)) {
-				bitVector.fastSet(featuresHashMap.get(token));
-				//System.out.println("Recognized method: " + token);
-				bw.write("R: " + token + "\n");
-				this.recCount++;
-				this.count++;
-			}
-			else{
-				//System.out.println("Unrecognized method: " + token);
-				bw.write("U: " + token + "\n");
-				this.unRecCount++;
-				this.count++;
+		  while((currentRecord = br.readLine()) != null){		
+			if (currentRecord.contains("invoke")){
+				tokens =  currentRecord.split(delimiter);
+				token = tokens[tokens.length-1];
+						
+				//System.out.println(token);
+				if (featuresHashMap.containsKey(token)) {
+					bitVector.fastSet(featuresHashMap.get(token));
+					//System.out.println("Recognized method: " + token);
+					bw.write("R: " + token + "\n");
+					this.recCount++;
+					this.count++;
+				}
+				else{
+					//System.out.println("Unrecognized method: " + token);
+					bw.write("U: " + token + "\n");
+					this.unRecCount++;
+					this.count++;
+				}
 			}
 		}
-	}
 
 	  
 
 	  bw.close();
 	  br.close();
+	  fw.close();
 	}
 	
 	
@@ -157,13 +164,8 @@ public class SmaliParser {
 		            
 		            if (!bitVector.isEmpty()){
 		            	//System.out.println(fileEntry.getName() + ", " + bitVector);
-		            	ByteArrayOutputStream baos = new ByteArrayOutputStream();  
-		    		    ObjectOutputStream oos = new ObjectOutputStream(baos); 
-		    		    oos.writeObject(bitVector);  
-		    		    oos.close();  
-		    		    byte[] bytes = baos.toByteArray();  
-		    		    bw.write(Hex.encodeHexString(bytes) + ", " + fileEntry.getName() + "\n");
-		            
+		            	bw.write(this.bitVectorToString(bitVector) + ", " + fileEntry.getName() + "\n");
+		            	bitVectorsHashMap.put(fileEntry.getName(), bitVector);
 		            }
 		            
 		        } else {
@@ -172,6 +174,7 @@ public class SmaliParser {
 		    } 
 		    
 		    bw.close();
+		    fw.close();
 		    
 		    
 		}catch (Exception e) {
@@ -200,6 +203,84 @@ public class SmaliParser {
 	            }
 	        }
 	    }
+	}
+	
+	public void compareBitVectors() {
+		
+		OpenBitSet bitVector1;
+		OpenBitSet bitVector2;
+		double jSim;
+		
+		try{
+			
+			File file = new File(this.outputSimFile);
+
+		  
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			
+
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+			BufferedWriter bw = new BufferedWriter(fw);
+		
+			 for(Iterator<Map.Entry<String, OpenBitSet>> iter1 = bitVectorsHashMap.entrySet().iterator(); iter1.hasNext(); ) {
+					Map.Entry<String, OpenBitSet> entry1 = iter1.next();
+					bitVector1 = entry1.getValue();
+					iter1.remove();
+					bw.write("Jaccard Similarity for " + entry1.getKey() + " against:\n");
+					
+					for(Iterator<Map.Entry<String, OpenBitSet>> iter2 = bitVectorsHashMap.entrySet().iterator(); iter2.hasNext(); ) {
+						Map.Entry<String, OpenBitSet> entry2 = iter2.next();
+						bitVector2 = entry2.getValue();
+						jSim = this.JaccardSim(bitVector1, bitVector2);
+						bw.write("\t" + entry2.getKey() + " = " + jSim + "\n");
+						
+						
+						if (jSim > jaccardThreshold)
+							System.out.println(entry1.getKey() + " vs " + entry2.getKey() + " = " + jSim);
+						
+					}
+			  }
+			 
+			 bw.close();
+			 fw.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String bitVectorToString(OpenBitSet bitVector){
+		try{
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+		    ObjectOutputStream oos = new ObjectOutputStream(baos); 
+		    oos.writeObject(bitVector);
+		    oos.close();  
+		    byte[] bytes = baos.toByteArray(); 
+		    return(Hex.encodeHexString(bytes));
+		    
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public double JaccardSim(OpenBitSet bitVector1, OpenBitSet bitVector2){
+		
+		OpenBitSet bitVectorIntersect = (OpenBitSet)bitVector1.clone();
+		OpenBitSet bitVectorUnion = (OpenBitSet)bitVector1.clone();
+		
+		bitVectorIntersect.intersect(bitVector2);
+		bitVectorUnion.union(bitVector2);
+		
+		double jaccardSim = (double)bitVectorIntersect.cardinality()/(double)bitVectorUnion.cardinality();
+		//System.out.println(bitVectorIntersect.cardinality() + " " + bitVectorUnion.cardinality() + " " + jaccardSim);
+		
+		return jaccardSim;
 	}
 
 
